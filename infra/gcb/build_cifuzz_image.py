6b7fbbcab0d0d8f0d19d80437eb88904e317d0b6
Leo Neat <leosneat@gmail.com>
2020-02-04 15:01:31 -0800

    Formatting and linting

--- infra/gcb/build_cifuzz_image.py
+++ infra/gcb/build_cifuzz_image.py
@@ -1,24 +1,40 @@
-#!/usr/bin/python2
-"""Build base images on Google Cloud Builder.
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Build the CIFuzz image on Google Cloud Builder.
 
-Usage: build_base_images.py
+Usage: build_cifuzz_images.py
 """
 
 import os
 import sys
 import yaml
 
+# pylint: disable=import-error
 from oauth2client.client import GoogleCredentials
 from googleapiclient.discovery import build
 
-
-CIFUZZ_IMAGE = 'base-cifuzz',
-
+CIFUZZ_IMAGE = 'base-cifuzz'
 PROJECT_ID = 'oss-fuzz-cifuzz'
 TAG_PREFIX = 'gcr.io/{0}/'.format(PROJECT_ID)
 
 
-def get_steps(ci_fuzz_image):
+def get_steps():
+  """Gets the required steps to build the ci-fuzz image on gcr.
+
+  Returns:
+    A list of steps to be executed on google cloud.
+  """
   steps = [{
       'args': [
           'clone',
@@ -42,18 +58,27 @@ def get_steps(ci_fuzz_image):
 
 
 def get_logs_url(build_id):
-  URL_FORMAT = ('https://console.developers.google.com/logs/viewer?'
+  """Gets the logs from the google cloud run instance.
+
+  Args:
+    build_id: The build id associated with the current build.
+
+  Returns:
+    A URL linking to the build logs.
+  """
+  url_format = ('https://console.developers.google.com/logs/viewer?'
                 'resource=build%2Fbuild_id%2F{0}&project={1}')
-  return URL_FORMAT.format(build_id, PROJECT_ID)
+  return url_format.format(build_id, PROJECT_ID)
 
 
 def main():
+  """Upload the CIFuzz docker image to Google container registry."""
   options = {}
   if 'GCB_OPTIONS' in os.environ:
     options = yaml.safe_load(os.environ['GCB_OPTIONS'])
 
   build_body = {
-      'steps': get_steps(CIFUZZ_IMAGE),
+      'steps': get_steps(),
       'timeout': str(4 * 3600) + 's',
       'options': options,
       'images': [TAG_PREFIX + CIFUZZ_IMAGE],
@@ -61,12 +86,11 @@ def main():
 
   credentials = GoogleCredentials.get_application_default()
   cloudbuild = build('cloudbuild', 'v1', credentials=credentials)
-  build_info = cloudbuild.projects().builds().create(
-      projectId=PROJECT_ID, body=build_body).execute()
+  build_info = cloudbuild.projects().builds().create(projectId=PROJECT_ID,
+                                                     body=build_body).execute()
   build_id = build_info['metadata']['build']['id']
 
-  print >> sys.stderr, 'Logs:', get_logs_url(build_id)
-  print build_id
+  print('Logs:', get_logs_url(build_id), file=sys.stderr)
 
 
 if __name__ == '__main__':
